d26c9d6e0f51b2663da39c8bf45379992bf74066
dean <deanraguso@gmail.com>
2021-04-29 16:13:26 +1000

    Finished up to pg. 50

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const title = "React APP";
+
+const welcome = {
+  greeting: "HEY",
+  title: "React"
+};
 
 function App() {
+  const stories = ["donkey ballz", "donkey teeth", "donkey tail"];
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  }
+
+  const searchResults = (stories.filter(function(story){
+    return story.toLowerCase().includes(searchTerm.toLowerCase());
+  }));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>{welcome.greeting} {welcome.title}</h1>
+      <Search handleChange={handleChange}/>
+        <List list={searchResults}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </p>
     </div>
   );
 }
+
+function Search(props) {
+
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={props.handleChange}/>
+    </div>
+  )
+}
+
+const List = props =>
+  props.list.map(item => (
+    <h2>{item}</h2>
+  ));
 
 export default App;
